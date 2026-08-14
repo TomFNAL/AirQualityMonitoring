4df9ee46b8ca8f4281fa53dd8665a4fed64eb873
Tom DeLine <101751424+TomFNAL@users.noreply.github.com>
2026-08-14 16:09:11 -0500

    Update zmqClient.py
    Apollo client script

--- zmqClient.py
+++ zmqClient.py
@@ -6,11 +6,19 @@ import re
 import sqlite3
 import time
 from datetime import datetime
+import logging
+
+#Configure logging
+logging.basicConfig(
+    filename="/tmp/AirGradient/AirGradient.log",
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s"
+)
+
+logger = logging.getLogger(__name__)
 
 #Connect to the AirGradient One over USB
 ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
-conn = sqlite3.connect("airgradient.db")
-cur = conn.cursor()
 data = {}
 
 #Name the device
@@ -19,7 +27,6 @@ DEVICE = "Apollo-AG1"
 
 #Configure the zmq client for the server
 SERVER_IP = "131.225.56.221"
-#TestServer = "0.0.0.0"
 context = zmq.Context()
 socket = context.socket(zmq.PUSH)
 socket.connect(f"tcp://{SERVER_IP}:5555")
@@ -97,12 +104,7 @@ while True:
 
         elif m := re.search(r'Particle Count 10\s*=\s*([\d.]+)', line):
                 data["count_100"] = float(m.group(1))
-
-                #timestamp = datetime.now().isoformat()
-                # end of measurement frame
-                #data["timestamp"] = datetime.now().isoformat(timespec="seconds")
-                #data["device"] = DEVICE
-                print(
+                logger.info(
                         f"{data['timestamp']} "
                         f"DeviceID: {data['device']} "
                         f"CO2={data.get('rco2')} "
@@ -126,18 +128,32 @@ while True:
                         f"CntPM10={data.get('count_100')}"
                 )
 
+# This print statement is for debuggin purposes. The logger captures the output
+# and writes it to /tmp/AirGradient/AirGradient.log
+#                print(
+#                        f"{data['timestamp']} "
+#                        f"DeviceID: {data['device']} "
+#                        f"CO2={data.get('rco2')} "
+#                        f"T={data.get('atmp')} "
+#                        f"RH={data.get('rhum')} "
+#                        f"TVOC={data.get('tvocIndex')} "
+#                        f"TVOC_Raw={data.get('tvocRaw')} "
+#                        f"NOx={data.get('noxIndex')} "
+#                        f"NOx_Raw={data.get('noxRaw')} "
+#                        f"AtmPM1.0={data.get('atm_pm01')} "
+#                        f"AtmPM2.5={data.get('atm_pm02')} "
+#                        f"AtmPM10={data.get('atm_pm10')} "
+#                        f"StdPM1.0={data.get('std_pm01')} "
+#                        f"StdPM2.5={data.get('std_pm02')} "
+#                        f"StdPM10={data.get('std_pm10')} "
+#                        f"CntPM0.3={data.get('count_03')} "
+#                        f"CntPM0.5={data.get('count_05')} "
+#                        f"CntPM1.0={data.get('count_10')} "
+#                        f"CntPM2.5={data.get('count_25')} "
+#                        f"CntPM5.0={data.get('count_50')} "
+#                        f"CntPM10={data.get('count_100')}"
+#                )
+
                 #print(data)
                 socket.send_json(data)
                 data = {}
-
-
-
-
-
-
-        #cereal_data = "This is my cereal data, with some commas added for flavor"
-        #print(f"Cereal Data = {cereal_data}")
-
-        #Send the serial data
-        #socket.send_string(cereal_data)
-        #time.sleep(1)
